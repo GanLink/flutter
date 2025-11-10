51563faad9cd1b91f645dd4c9ca6078a84b2996f
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-11-09 19:53:06 -0500

    feat(auth): add dependecies and files for theme

---
+++ lib/core/ui/theme.dart
@@ -0,0 +1,76 @@
+// lib/theme/theme.dart
+import 'package:flutter/material.dart';
+import 'type.dart';
+import 'color.dart';
+
+
+final lightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    primary: primaryLight,
+    onPrimary: onPrimaryLight,
+    primaryContainer: primaryContainerLight,
+    onPrimaryContainer: onPrimaryContainerLight,
+    secondary: secondaryLight,
+    onSecondary: onSecondaryLight,
+    secondaryContainer: secondaryContainerLight,
+    onSecondaryContainer: onSecondaryContainerLight,
+    tertiary: tertiaryLight,
+    onTertiary: onTertiaryLight,
+    tertiaryContainer: tertiaryContainerLight,
+    onTertiaryContainer: onTertiaryContainerLight,
+    error: errorLight,
+    onError: onErrorLight,
+    errorContainer: errorContainerLight,
+    onErrorContainer: onErrorContainerLight,
+    background: backgroundLight,
+    onBackground: onBackgroundLight,
+    surface: surfaceLight,
+    onSurface: onSurfaceLight,
+    surfaceVariant: surfaceVariantLight,
+    onSurfaceVariant: onSurfaceVariantLight,
+    outline: outlineLight,
+    inverseSurface: inverseSurfaceLight,
+    onInverseSurface: inverseOnSurfaceLight,
+    inversePrimary: inversePrimaryLight,
+    scrim: scrimLight,
+  ),
+  textTheme: textTheme,
+);
+
+final darkTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.dark,
+    primary: primaryDark,
+    onPrimary: onPrimaryDark,
+    primaryContainer: primaryContainerDark,
+    onPrimaryContainer: onPrimaryContainerDark,
+    secondary: secondaryDark,
+    onSecondary: onSecondaryDark,
+    secondaryContainer: secondaryContainerDark,
+    onSecondaryContainer: onSecondaryContainerDark,
+    tertiary: tertiaryDark,
+    onTertiary: onTertiaryDark,
+    tertiaryContainer: tertiaryContainerDark,
+    onTertiaryContainer: onTertiaryContainerDark,
+    error: errorDark,
+    onError: onErrorDark,
+    errorContainer: errorContainerDark,
+    onErrorContainer: onErrorContainerDark,
+    background: backgroundDark,
+    onBackground: onBackgroundDark,
+    surface: surfaceDark,
+    onSurface: onSurfaceDark,
+    surfaceVariant: surfaceVariantDark,
+    onSurfaceVariant: onSurfaceVariantDark,
+    outline: outlineDark, 
+    inverseSurface: inverseSurfaceDark,
+    onInverseSurface: inverseOnSurfaceDark,
+    inversePrimary: inversePrimaryDark,
+    scrim: scrimDark,
+    // ...y así sucesivamente para el tema oscuro
+  ),
+  textTheme: textTheme,
+);
